72a412d129331b5b64242ba2919a2969fdbbdce7
Norjamille <122094177+Norjamille@users.noreply.github.com>
2023-01-14 16:18:26 +0800

    Staff Update

--- resources/js/Pages/Admin/Dashboard/Index.vue
+++ resources/js/Pages/Admin/Dashboard/Index.vue
@@ -4,8 +4,9 @@ import AdminLayout from '@/Layouts/AdminLayout.vue'
 
 <template>
    <AdminLayout title="Dashboard">
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ad ducimus eos mollitia. Vero illum eveniet odio reprehenderit magnam, adipisci unde ipsam expedita eum sint ratione iure debitis alias? Amet.
-        </div>
+      <div>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ad ducimus eos mollitia. Vero illum eveniet odio
+         reprehenderit magnam, adipisci unde ipsam expedita eum sint ratione iure debitis alias? Amet.
+      </div>
    </AdminLayout>
 </template>
